fa35ccb0181c4f75f92fbeafe7106e0c104adf11
Sam Shock <sam.shock924@gmail.com>
2022-06-14 16:20:33 -0400

    misc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import SearchBox from "./Components/SearchBox";
-// import CharacterData from "./Components/CharacterData";
+import CharacterData from "./Components/CharacterData";
 import CharacterTable from "./Components/CharacterTable";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<h1 className="title">Character Archives</h1>
 			<SearchBox />
-			{/* <CharacterData /> */}
+			<CharacterData />
 			<CharacterTable />
 		</div>
 	);
--- src/Components/CharacterTable.js
+++ src/Components/CharacterTable.js
@@ -3,32 +3,31 @@ import Table from "react-bootstrap/Table";
 
 const characterDataTable = () => {
 	return (
-		<Table striped bordered hover className="characterDataTable">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Birthday</th>
-					<th>Height</th>
-					<th>Weight</th>
-					<th>Homeworld</th>
-					<th>Species</th>
-				</tr>
-			</thead>
-			<tbody>
-				{CharacterData.map((character, index) => {
-					return (
-						<tr key={index}>
-							<td>{character.name}</td>
-							<td>{character.birth_year}</td>
-							<td>{character.height}</td>
-							<td>{character.mass}</td>
-							<td>{character.homeworld}</td>
-							<td>{character.species}</td>
-						</tr>
-					);
-				})}
-			</tbody>
-		</Table>
+		<div>
+			<Table striped bordered hover className="characterDataTable">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Birthday</th>
+						<th>Height</th>
+						<th>Weight</th>
+						<th>Homeworld</th>
+						<th>Species</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						{/* <CharacterData /> */}
+						<td>luke</td>
+						<td>never</td>
+						<td>5'1"</td>
+						<td>155lb</td>
+						<td>earth</td>
+						<td>human</td>
+					</tr>
+				</tbody>
+			</Table>
+		</div>
 	);
 };
 
