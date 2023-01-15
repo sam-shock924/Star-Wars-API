c0b00f6636a785796a0b1880dc0110122419f4d2
Sam Shock <sam.shock924@gmail.com>
2023-01-15 15:10:09 -0500

    static shows on screen correctly

--- src/Components/CharacterTable.js
+++ src/Components/CharacterTable.js
@@ -1,50 +1,38 @@
 import Table from 'react-bootstrap/Table';
 
 const CharacterTable = ({ data, setData }) => {
+	const characterDataRow = data.map((character) => (
+		<>
+			<tr key={character.created}>
+				<td>{character.name}</td>
+				<td>{character.birth_year}</td>
+				<td>{character.height}</td>
+				<td>{character.mass}</td>
+				<td>{character.homeworld}</td>
+				<td>{character.species}</td>
+			</tr>
+		</>
+	));
+
 	return (
 		<div>
 			<Table striped bordered hover className='characterDataTable'>
 				<thead>
 					<tr>
 						<th>Name</th>
-						<th>Birth Date</th>
+						<th className='character-birth-year'>Birth Year*</th>
 						<th>Height</th>
 						<th>Mass</th>
 						<th>Home World</th>
 						<th>Species</th>
 					</tr>
 				</thead>
-				<tbody>
-					<tr>
-						<div>
-							{data.map((character) => (
-								<div>
-									<div>
-										<td>{character.name}</td>
-									</div>
-									<div>
-										<td>{character.height}</td>
-									</div>
-									<div>
-										<td>{character.mass}</td>
-									</div>
-								</div>
-							))}
-						</div>
-					</tr>
-
-					{/* <tr>
-						<td>{props.data.name}</td>
-						<td>01/01/01</td>
-						<td>{props.height}</td>
-						<td>{props.mass}</td>
-						<td>Home World</td>
-						<td>Species</td>
-					</tr> */}
-				</tbody>
+				<tbody>{characterDataRow}</tbody>
 			</Table>
 		</div>
 	);
 };
 
 export default CharacterTable;
+
+//work on unique id keys for each td
