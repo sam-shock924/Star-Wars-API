82e5f5363dbf3a9bb84130e3279fb00e98b3736b
Sam Shock <sam.shock924@gmail.com>
2023-01-15 14:27:49 -0500

    static data showing on screen...mostly

--- src/Components/CharacterData.js
+++ src/Components/CharacterData.js
@@ -1,21 +1,17 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const dataApi = 'https://swapi.dev/api/people/';
 
 export default function CharacterData({ data, setData }) {
 	useEffect(() => {
-		// const [chars, setChars] = useState([]);
 		axios
 			.get(dataApi)
 			.then((response) => {
 				console.log(response.data.results);
-				// setChars(response.data);
 			})
 			.catch((error) => {
 				console.log(error);
 			});
 	}, []);
-
-	// return <div>{data}</div>;
 }
