38d25780e3073a4ad896e0e1dbaa7f541d2222f2
Sam Shock <sam.shock924@gmail.com>
2023-01-18 12:27:20 -0500

    stuck like glue here

--- src/Components/CharacterData.js
+++ src/Components/CharacterData.js
@@ -4,11 +4,11 @@ import { useEffect } from 'react';
 const dataApi = 'https://swapi.dev/api/people/';
 
 export default function CharacterData({ data, setData }) {
-	useEffect((setData) => {
+	useEffect(() => {
 		axios
 			.get(dataApi)
-			.then((response) => response.json())
-			.then((json) => setData(json))
+			.then((response) => console.log(response.data.results))
+			.then((response) => setData(response.data.results))
 			.catch((error) => {
 				console.log(error);
 			});
